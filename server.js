8e6ae762e260f69c4fcc3ad32329f51e3e1f04da
Christopher Vachon <code@christophervachon.com>
2015-05-05 22:19:09 -0400

    Update to create a config file if not found and populate from process.env

--- server.js
+++ server.js
@@ -11,26 +11,37 @@ app.use(express.static(__dirname + '/wwwroot'));
 
 app.post("/send", jsonBodyParser, urlencode, function(request, response) {
     fs.readFile(__dirname + "/settings.json", "utf-8", function(error, contents) {
+        var settings;
         if (error) {
             console.log(error);
-            response.status(500).json("error");
+            settings = {
+                "mailer": {
+                    "service": "gmail",
+                    "auth": {
+                        "user": process.env.maileruser || "ENV.maileruser",
+                        "pass": process.env.mailerpassword || "ENV.mailerpassword"
+                    }
+                },
+                "sendMailTo": process.env.mailersend || "ENV.mailersendto"
+            }
+            fs.writeFileSync(__dirname + "/settings.json", JSON.stringify(settings, null, "\t"));
         } else {
-            var settings = JSON.parse(contents);
-            var transporter = nodemailer.createTransport(settings.mailer);
-            transporter.sendMail({
-                from: request.body.emailAddress,
-                to: settings.sendMailTo,
-                subject: request.body.subject,
-                text: request.body.message
-            }, function(error, info) {
-                if (error) {
-                    console.log(error);
-                    response.status(500).json("error");
-                } else {
-                    response.json("ok");
-                }
-            });
+            settings = JSON.parse(contents);
         }
+        var transporter = nodemailer.createTransport(settings.mailer);
+        transporter.sendMail({
+            from: request.body.emailAddress,
+            to: settings.sendMailTo,
+            subject: request.body.subject,
+            text: request.body.message
+        }, function(error, info) {
+            if (error) {
+                console.log(error);
+                response.status(500).json("error");
+            } else {
+                response.json("ok");
+            }
+        });
     });
 });
 
