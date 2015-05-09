846da1f556a852946971d8166ceb2c50651bde81
Christopher Vachon <code@christophervachon.com>
2015-05-08 20:12:34 -0400

    Prevent mailing from external sources

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var express = require('express'),
 app.use(express.static(__dirname + '/wwwroot'));
 
 app.post("/send", jsonBodyParser, urlencode, function(request, response) {
+    // Deny any Request thats not comeing from myself
+    if (!request.headers.referer || (request.headers.referer.indexOf(request.hostname) == -1)) {
+        response.status(401).json('Not Authorized to Mail');
+        return;
+    }
+
     fs.readFile(__dirname + "/settings.json", "utf-8", function(error, contents) {
         var settings;
         if (error) {
@@ -27,13 +33,17 @@ app.post("/send", jsonBodyParser, urlencode, function(request, response) {
             fs.writeFileSync(__dirname + "/settings.json", JSON.stringify(settings, null, "\t"));
         } else {
             settings = JSON.parse(contents);
+            if (process.env.maileruser) { settings.mailer.auth.user = process.env.maileruser; }
+            if (process.env.mailerpassword) { settings.mailer.auth.pass = process.env.mailerpassword; }
+            if (process.env.mailersendto) { settings.sendMailTo = process.env.mailersendto; }
         }
         var transporter = nodemailer.createTransport(settings.mailer);
         transporter.sendMail({
-            from: request.body.emailAddress,
+            replyTo: request.body.emailAddress,
             to: settings.sendMailTo,
-            subject: request.body.subject,
-            text: request.body.message
+            subject: request.body.subject || "Message From the Website",
+            text: "Message From the FineAlley Website\n\nFrom: " + request.body.name + " (" + request.body.emailAddress + ")\n\nMessage:\n\n" + request.body.message
+            //html: "<h1>Message from Website</h1><strong>From:</strong><br/>"
         }, function(error, info) {
             if (error) {
                 console.log(error);
